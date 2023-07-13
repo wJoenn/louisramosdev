7f875e3ca4d72f6f39919998aabd7aca8cc6245f
Louis Ramos <louisramosdev@gmail.com>
2023-07-13 19:23:34 +0200

    Replaced logo and favicon

--- components/NuxtHeader.vue
+++ components/NuxtHeader.vue
@@ -1,7 +1,10 @@
 <template>
   <header>
     <div class="container">
-      <img src="@/assets/images/logos/nuxt.png" alt="">
+      <div class="logo">
+        <div><div /></div>
+        <div><div /></div>
+      </div>
 
       <nav>
         <NuxtLink to="/" :class="{ current: route.name === 'index' }">Home</NuxtLink>
@@ -21,6 +24,20 @@
     align-items: center;
     display: flex;
     gap: 20px;
+  }
+
+  @keyframes rotate {
+    0% {
+      rotate: 0deg;
+    }
+
+    50% {
+      rotate: 180deg;
+    }
+
+    100% {
+      rotate: 360deg;
+    }
   }
 
   header {
@@ -52,6 +69,35 @@
           &:hover {
             color: $main-color;
           }
+        }
+      }
+    }
+
+    .logo {
+      gap: 0;
+
+      > div {
+        animation: rotate 20s linear infinite;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        height: 33px;
+        width: 33px;
+        background-image: linear-gradient(135deg, $light-nuxt-green, mediumpurple);
+        border-radius: 50%;
+        box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
+
+        &:last-child {
+          margin-left: -10px;
+          z-index: -10;
+        }
+
+        div {
+          height: 28px;
+          width: 28px;
+          background-color: $main-background;
+          border-radius: 50%;
+          z-index: 10;
         }
       }
     }
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      link: [
+        { rel: "icon", type: "image/x-icon", href: "/favicon.png" }
+      ]
+    }
+  },
   build: {
     transpile: [
       "@fortawesome/vue-fontawesome",
