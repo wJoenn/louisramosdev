107c55844b7a7c7888b6be80d91e02fca426f7db
Louis Ramos <louisramosdev@gmail.com>
2023-07-23 11:36:56 +0200

    Fixed Stack not loading bug on mobile

--- pages/stack.vue
+++ pages/stack.vue
@@ -6,8 +6,8 @@
     <p>I figured it might interest some people to learn about what I decided to use. There's nothing too extravagent yet, so far it's mostly just Vue and Ruby, but who knows what I might end up with later.</p>
 
     <ClientOnly>
-      <Transition appear name="frontend" @before-enter="beforeEnter" @enter="enter">
-        <section id="frontend" data-delay="0">
+      <Transition appear @before-enter="beforeEnter" @enter="enter">
+        <section v-if="frontendTools" id="frontend" data-delay="0">
           <div class="title">
             <h2>Frontend</h2>
             <h4>This is the Stack that's being used for this website</h4>
@@ -21,23 +21,23 @@
         </section>
       </Transition>
 
-      <Transition appear name="backend" @before-enter="beforeEnter" @enter="enter">
-        <section id="backend" :data-delay="frontendTools.length">
+      <Transition appear @before-enter="beforeEnter" @enter="enter">
+        <section v-if="backendTools" id="backend" :data-delay="frontendTools?.length">
           <h2>Backend</h2>
 
           <TransitionGroup appear tag="ul" @before-enter="beforeEnter" @enter="enter">
-            <li v-for="(tool, index) in backendTools" :key="tool.name" :data-delay="index + frontendTools.length">
+            <li v-for="(tool, index) in backendTools" :key="tool.name" :data-delay="index + (frontendTools?.length || 0)">
               <Tool :tool="tool" />
             </li>
           </TransitionGroup>
         </section>
       </Transition>
 
-      <Transition appear name="other" @before-enter="beforeEnter" @enter="enter">
-        <section id="other" :data-delay="frontendTools.length + backendTools.length">
+      <Transition appear @before-enter="beforeEnter" @enter="enter">
+        <section v-if="otherTools" id="other" :data-delay="(frontendTools?.length || 0) + (backendTools?.length || 0)">
           <h2>Other Tools</h2>
           <TransitionGroup appear tag="ul" @before-enter="beforeEnter" @enter="enter">
-            <li v-for="(tool, index) in otherTools" :key="tool.name" :data-delay="index + frontendTools.length + backendTools.length">
+            <li v-for="(tool, index) in otherTools" :key="tool.name" :data-delay="index + (frontendTools?.length || 0) + (backendTools?.length || 0)">
               <Tool :tool="tool" />
             </li>
           </TransitionGroup>
@@ -45,7 +45,7 @@
       </Transition>
 
       <Transition appear name="foot" @before-enter="beforeEnter" @enter="enter">
-        <p :data-delay="frontendTools.length + backendTools.length + otherTools.length">There are more tools that I'd like to get into in the near future like <a href="https://vitest.dev/" target="_blank">Vitest</a>, <a href="https://nodejs.org/en" target="_blank">Node.js</a>, <a href="https://www.docker.com/" target="_blank">Docker</a> and <a href="https://go.dev/" target="_blank">Go</a> but that's it for now.</p>
+        <p v-if="data" :data-delay="(frontendTools?.length || 0) + (backendTools?.length || 0) + (otherTools?.length || 0)">There are more tools that I'd like to get into in the near future like <a href="https://vitest.dev/" target="_blank">Vitest</a>, <a href="https://nodejs.org/en" target="_blank">Node.js</a>, <a href="https://www.docker.com/" target="_blank">Docker</a> and <a href="https://go.dev/" target="_blank">Go</a> but that's it for now.</p>
       </Transition>
     </ClientOnly>
   </div>
@@ -62,7 +62,9 @@
   })
 
   const { data } = await useFetch("/api/tools")
-  const { frontendTools, backendTools, otherTools } = toRefs(data.value!)
+  const frontendTools = computed(() => data.value?.frontendTools)
+  const backendTools = computed(() => data.value?.backendTools)
+  const otherTools = computed(() => data.value?.otherTools)
 
   const beforeEnter = (element: Element) => {
     const htmlElement = element as HTMLElement
