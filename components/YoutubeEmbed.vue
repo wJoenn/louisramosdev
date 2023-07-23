e3be6d59226628c5ac78a73c802a4753a647442b
Louis Ramos <louisramosdev@gmail.com>
2023-07-23 11:32:10 +0200

    Created YoutubeEmbed component

---
+++ components/YoutubeEmbed.vue
@@ -0,0 +1,72 @@
+<template>
+  <div id="modal" @click.self="emit('close')">
+    <button @click="emit('close')">
+      <fai icon="fa-solid fa-xmark" />
+    </button>
+
+    <iframe :src="src" />
+  </div>
+</template>
+
+<script setup lang="ts">
+  const emit = defineEmits(["close"])
+  defineProps<{
+    src: string
+  }>()
+
+  const closeEmbed = (event: KeyboardEvent) => {
+    if (event.key === "Escape") { emit("close") }
+  }
+
+  onMounted(() => {
+    const app = document.querySelector("#app") as HTMLDivElement
+    app.style.overflowY = "hidden"
+
+    window.addEventListener("keyup", closeEmbed)
+  })
+
+  onUnmounted(() => {
+    window.removeEventListener("keyup", closeEmbed)
+
+    const app = document.querySelector("#app") as HTMLDivElement
+    app.style.overflowY = "scroll"
+  })
+</script>
+
+<style scoped lang="scss">
+  #modal {
+    align-items: center;
+    background-color: rgba(24, 24, 24, 0.9);
+    display: flex;
+    height: 100vh;
+    justify-content: center;
+    position: absolute;
+    right: 0px;
+    top: 0px;
+    width: 100%;
+    z-index: 20;
+
+    button {
+      background-color: transparent;
+      border: none;
+      color: $secondary-color;
+      cursor: pointer;
+      position: fixed;
+      right: 20px;
+      top: 20px;
+      transition: color 0.3s ease;
+      width: 20px;
+
+      &:hover {
+        color: $main-color;
+      }
+    }
+
+    iframe {
+      aspect-ratio: 3/2;
+      border: none;
+      box-shadow: 0 0 20px rgba(0, 0, 0, 0.5);
+      width: Min(1080px, 90%);
+    }
+  }
+</style>
--- plugins/fontawesome.ts
+++ plugins/fontawesome.ts
@@ -1,13 +1,11 @@
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome"
 import { library, config } from "@fortawesome/fontawesome-svg-core"
-
-import { faAt, faCheckDouble, faCopy, faRotateLeft } from "@fortawesome/free-solid-svg-icons"
-
+import { faAt, faCheckDouble, faCopy, faRotateLeft, faXmark } from "@fortawesome/free-solid-svg-icons"
 import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons"
 
 config.autoAddCss = false
 
-library.add(faAt, faCheckDouble, faCopy, faRotateLeft, faGithub, faLinkedin)
+library.add(faAt, faCheckDouble, faCopy, faRotateLeft, faXmark, faGithub, faLinkedin)
 
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.component("fai", FontAwesomeIcon)
