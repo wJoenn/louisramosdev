01261bfd28f0fd0b5ff825f4c009c99badd4e913
Louis Ramos <louisramosdev@gmail.com>
2023-09-29 22:05:45 +0200

    Fix missing hover effect on github reactions

--- components/hub/GithubReactions.vue
+++ components/hub/GithubReactions.vue
@@ -156,6 +156,11 @@
         display: flex;
         justify-content: center;
         padding: 0.25rem 0.5rem;
+        transition: background-color 0.3s ease;
+
+        &:hover {
+          background-color: $secondary-background;
+        }
 
         &.pop-up {
           position: relative;
