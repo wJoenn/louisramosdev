f8c52d0bffdfe67b29d42a63e2e5b310883334a6
Louis Ramos <louisramosdev@gmail.com>
2023-10-01 10:29:15 +0200

    Fixed releases not being fetched in SSG mode

--- pages/hub/index.vue
+++ pages/hub/index.vue
@@ -19,7 +19,7 @@
 
   const env = useRuntimeConfig()
 
-  const { data } = await useFetch<{ releases: GhRelease[] }>(`${env.public.apiUrl}/github_releases`)
+  const { data } = await useFetch<{ releases: GhRelease[] }>(`${env.public.apiUrl}/github_releases`, { server: false })
 
   const password = ref("")
   const isLoggedIn = ref(false)
